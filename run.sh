12a24f61faac1995b828e4295d68396ef87351fa
Liam Elmore <lelmore@shopkeep.com>
2018-03-31 09:21:21 +0100

    Update error message text in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $# -eq 0 ] ; then
-    echo "Error: Please supply the number of commits you would like as an arugment."
+    echo "Error: Please supply the number of commits you would like as an arugment"
     exit
 fi
 
