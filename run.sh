648fcad41a52796bad5530f64c3a6ace7355a227
Liam Elmore <lelmore@shopkeep.com>
2018-03-31 09:09:24 +0100

    Update file with 1

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-max=100
+max=2
 for i in `seq 1 $max`
 do
-    echo "$i"
     echo "$i" > playdoh.txt
+    git commit -am "Update file with $i"
 done
-
 #echo '1' > playdoh.txt
-cat playdoh.txt
+#cat playdoh.txt
