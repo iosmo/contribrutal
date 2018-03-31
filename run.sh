6501193fc267a20de92c1a67d0386b6b46b226cd
Liam Elmore <lelmore@shopkeep.com>
2018-03-31 09:18:36 +0100

    Update file with 1

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-max=2
+if [ $# -eq 0 ] ; then
+    echo "Error: Please supply the number of commits you would like as an arugment."
+    exit
+fi
+
+max=$1
 for i in `seq 1 $max`
 do
     echo "$i" > playdoh.txt
